b8c751980b27b94d75c1f26f36bdacb6ad694850
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-11-12 10:29:22 -0500

    basic example list

--- main.go
+++ main.go
@@ -2,87 +2,109 @@ package main
 
 import (
 	"fmt"
-	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"os"
 )
 
-type status int
-
-const (
-	todo status = iota
-	inProgress
-	done
-)
-
-/*
-  CUSTOM ITEM
-*/
-
-type Task struct {
-	status      status
-	title       string
-	description string
+type model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
 }
 
-// implement the list.item interface
-func (t Task) FilterValue() string {
-	return t.title
+func initialModel() model {
+	return model{
+		// Our to-do list is a grocery list
+		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+
+		// A map which indicates which choices are selected. We're using
+		// the  map like a mathematical set. The keys refer to the indexes
+		// of the `choices` slice, above.
+		selected: make(map[int]struct{}),
+	}
 }
 
-func (t Task) Title() string {
-	return t.title
-}
-
-func (t Task) Description() string {
-	return t.description
-}
-
-/* MAIN MODEL */
-
-type Model struct {
-	list list.Model
-	err  error
-}
-
-func New() *Model {
-	return &Model{}
-}
-
-// TODO: call this on tea.WindowSizeMsg
-func (m *Model) InitList(width, height int) {
-	// width and height are the terminal values
-	m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
-	m.list.Title = "To Do"
-	m.list.SetItems([]list.Item{
-		Task{status: todo, title: "buy milk", description: "strawberry"},
-	})
-}
-
-func (m Model) Init() tea.Cmd {
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
 	return nil
 }
 
-func (m Model) Udpate(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.InitList(msg.Width, msg.Height)
+
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
 	}
-	// var cmd tea.Cmd
-	// m.list, cmd = m.list.Update(msg)
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
 	return m, nil
 }
 
-func (m Model) View() string {
-	return m.list.View()
+func (m model) View() string {
+	// The header
+	s := "What should we buy at the market?\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
 }
 
 func main() {
-	m := New()
-	p := tea.NewProgram(m)
-	_, err := p.Run()
-	if err != nil {
-		fmt.Println(err)
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
 }
